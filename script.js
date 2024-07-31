2843c3cb84b2be4231481a90e6031618c07de0f2
Faizan <149582509+faiz4n@users.noreply.github.com>
2024-07-31 19:39:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const topicArray = document.querySelectorAll(".topic");
+const dialogStarted = document.querySelector(".dialog-started");
+const dialogFinished = document.querySelector(".dialog-finished");
+const close1 = document.querySelector(".close-dialog-s");
+const yes1 = document.querySelector(".yes-btn-s");
+const close2 = document.querySelector(".close-dialog-f");
+const yes2 = document.querySelector(".yes-btn-f");
+let currentTopic;
+
+topicArray.forEach(topic =>{
+    
+    topic.addEventListener("click", ()=>{
+        currentTopic=topic;
+        if (!currentTopic.classList.contains("topic-started") && !currentTopic.classList.contains("topic-finished")) {
+            dialogStarted.showModal();
+        } else if (currentTopic.classList.contains("topic-started")) {
+            dialogFinished.showModal();
+        }
+    })
+})
+yes1.addEventListener("click", () => {
+    currentTopic.classList.add("topic-started");
+    dialogStarted.close();
+});
+
+close1.addEventListener("click", () => {
+    dialogStarted.close();
+});
+
+yes2.addEventListener("click", () => {
+    currentTopic.classList.add("topic-finished");
+    dialogFinished.close();
+});
+
+close2.addEventListener("click", () => {
+    dialogFinished.close();
+});
+
