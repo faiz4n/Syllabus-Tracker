d1d1f1c643d3fada2641df184ccf267399a617b4
Faizan <149582509+faiz4n@users.noreply.github.com>
2024-08-02 15:30:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,20 @@ const yes1 = document.querySelector(".yes-btn-s");
 const close2 = document.querySelector(".close-dialog-f");
 const yes2 = document.querySelector(".yes-btn-f");
 let currentTopic;
+let topicExpanded;
 
 topicArray.forEach(topic =>{
     
+    topicExpanded = document.querySelector(".topic-expanded");
     topic.addEventListener("click", ()=>{
         currentTopic=topic;
         if (!currentTopic.classList.contains("topic-started") && !currentTopic.classList.contains("topic-finished")) {
             dialogStarted.showModal();
-        } else if (currentTopic.classList.contains("topic-started")) {
+        } else if (currentTopic.classList.contains("topic-started") && !currentTopic.classList.contains("topic-finished")) {
             dialogFinished.showModal();
+        }
+        else if(currentTopic.classList.contains("topic-started") && currentTopic.classList.contains("topic-finished")){
+            topicExpanded.classList.toggle("expanded")
         }
     })
 })
@@ -35,4 +40,3 @@ yes2.addEventListener("click", () => {
 close2.addEventListener("click", () => {
     dialogFinished.close();
 });
-
